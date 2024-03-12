c5559b36b50e5d1b4f817235b9561e8208b289d4
Giulio Avalos <giulio_avalos@hotmail.es>
2024-03-12 14:54:48 -0300

    added controller

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,7 @@
+# app/models/bookmark.rb
 class Booking < ApplicationRecord
+  belongs_to :court
+  belongs_to :user
+
+  validates :date, :beginning_time, :ending_time, :court_id, :user_id, presence: true
 end
